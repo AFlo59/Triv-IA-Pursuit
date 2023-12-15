e41ab10804cd95622caee34d6e858aea7c890e9c
AFlo59 <etudessup59230@gmail.com>
2023-12-15 11:42:40 +0100

    add test for inscription and change Joueur&Partie

---
+++ model/test_inscription.py
@@ -0,0 +1,12 @@
+from Partie import Partie
+partie = Partie()
+
+partie.inscription("Flo")
+partie.inscription("Player2")
+partie.inscription("Player3")
+partie.inscription("Florian")
+partie.inscription("Player5")
+partie.inscription("Player6")
+partie.inscription("Player7")
+
+partie.afficher_joueurs()
